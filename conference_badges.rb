cb3d7d915590846889ed7f736947cdfdb7eeb55b
Joe Quattrone <joequattrone5@gmail.com>
2018-11-27 22:16:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def badge_creator(attendees)
+def batch_badge_creator(attendees)
   attendees.each do |attendee|
     "Hello, my name is #{attendee}"
   end
