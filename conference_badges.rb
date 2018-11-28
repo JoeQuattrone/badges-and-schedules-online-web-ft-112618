5ed12125ee77612478f96986c9b0cc3fe5956128
Joe Quattrone <joequattrone5@gmail.com>
2018-11-28 14:02:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+     "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(attendees)
+  new_array = []
+  attendees.each_with_index do |attendee, index|
+    new_array << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  end
+  new_array
+end
+
+def printer(attendees)
+  assign_rooms(attendees).each do |y|
+    puts y
+  end
+  
+    batch_badge_creator(attendees).each do |x|
+     puts x
+  end
+end
